f5f879a12d583b57bc272d49c4c10bf03304ff11
KovalenkoILja <dalekake@gmail.com>
2019-12-24 08:28:31 +0300

    move init_contents to separate module

--- Application.py
+++ Application.py
@@ -1,10 +1,10 @@
 import os
 
-import magenta.music as mm
-
 import input
+from modules.common.create_dir import check_dir
 from modules.common.print_version import print_versions
 from modules.common.user_input import wait_for_user_input
+from modules.generation.check_contents import init_contents
 from modules.generation.generate_continue import generate_continue_for
 from modules.generation.generate_interpolation import generate_interpolation_for
 from modules.generation.initialisation_vae import init_music_vae
@@ -17,7 +17,7 @@ class Application:
 
     def start(self):
 
-        self.init_contents()
+        init_contents()
 
         while True:
             self.print_controls()
@@ -61,7 +61,7 @@ class Application:
                 generate_continue_for("teapot", input_sequence, model_name, num_steps, temperature)
 
             elif input.user_input == "4":
-                print("4 input")
+                print()
 
                 if not os.path.isfile("./content/cat-mel_2bar_big.ckpt.data-00000-of-00001") \
                         or not os.path.isfile("./content/cat-mel_2bar_big.ckpt.index"):
@@ -79,6 +79,8 @@ class Application:
 
             elif input.user_input == "5":
                 print("5 input")
+                path = "./input"
+                check_dir(path)
 
             # elif input.user_input == "6":
             #     print("6 input")
@@ -96,40 +98,13 @@ class Application:
                 break
 
     @staticmethod
-    def init_contents():
-        if os.path.exists("./content"):
-            if not os.path.isfile("./content/basic_rnn.mag"):
-                print("basic_rnn.mag doesnt find!")
-                mm.notebook_utils.download_bundle('basic_rnn.mag', './content/')
-                print("basic_rnn.mag successfully downloaded")
-            if not os.path.isfile("./content/mono_rnn.mag"):
-                print("mono_rnn.mag doesnt find!")
-                mm.notebook_utils.download_bundle('mono_rnn.mag', './content/')
-                print("mono_rnn.mag successfully downloaded")
-            if not os.path.isfile("./content/lookback_rnn.mag"):
-                print("lookback_rnn.mag doesnt find!")
-                mm.notebook_utils.download_bundle('lookback_rnn.mag', './content/')
-                print("lookback_rnn.mag successfully downloaded")
-            if not os.path.isfile("./content/attention_rnn.mag"):
-                print("attention_rnn.mag doesnt find!")
-                mm.notebook_utils.download_bundle('attention_rnn.mag', './content/')
-                print("attention_rnn.mag successfully downloaded")
-            if not os.path.isfile("./content/cat-mel_2bar_big.ckpt.data-00000-of-00001") \
-                    or not os.path.isfile("./content/cat-mel_2bar_big.ckpt.index"):
-                print("models for interpolate doesnt find!")
-        else:
-            print("content folder doesnt find!")
-            exit(1)
-        print("all bundles set up")
-
-    @staticmethod
     def print_controls():
         print("Press key:")
         print("1: create sample midi files")
         print("2: generate continue for twinkle_twinkle")
         print("3: generate continue for teapot")
         print("4: generate interpolating between twinkle_twinkle and teapot")
-        print("5: ")
+        print("5: generate continue for files in input folder")
         # print("6: ")
         # print("7: ")
         # print("8: ")
---
+++ modules/generation/check_contents.py
@@ -0,0 +1,30 @@
+import os
+
+import magenta.music as mm
+
+
+def init_contents():
+    if os.path.exists("./content"):
+        if not os.path.isfile("./content/basic_rnn.mag"):
+            print("basic_rnn.mag doesnt find!")
+            mm.notebook_utils.download_bundle('basic_rnn.mag', './content/')
+            print("basic_rnn.mag successfully downloaded")
+        if not os.path.isfile("./content/mono_rnn.mag"):
+            print("mono_rnn.mag doesnt find!")
+            mm.notebook_utils.download_bundle('mono_rnn.mag', './content/')
+            print("mono_rnn.mag successfully downloaded")
+        if not os.path.isfile("./content/lookback_rnn.mag"):
+            print("lookback_rnn.mag doesnt find!")
+            mm.notebook_utils.download_bundle('lookback_rnn.mag', './content/')
+            print("lookback_rnn.mag successfully downloaded")
+        if not os.path.isfile("./content/attention_rnn.mag"):
+            print("attention_rnn.mag doesnt find!")
+            mm.notebook_utils.download_bundle('attention_rnn.mag', './content/')
+            print("attention_rnn.mag successfully downloaded")
+        if not os.path.isfile("./content/cat-mel_2bar_big.ckpt.data-00000-of-00001") \
+                or not os.path.isfile("./content/cat-mel_2bar_big.ckpt.index"):
+            print("models for interpolate doesnt find!")
+    else:
+        print("content folder doesnt find!")
+        exit(1)
+    print("all bundles set up")
